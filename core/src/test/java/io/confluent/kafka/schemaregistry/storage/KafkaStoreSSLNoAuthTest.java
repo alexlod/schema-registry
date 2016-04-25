2daba4662fb202c26fac216336ae22a5ff47e614
Alex Loddengaard <alexloddengaard@gmail.com>
2016-04-25 13:38:44 -0700

    KafkaStore SSL support.

---
+++ core/src/test/java/io/confluent/kafka/schemaregistry/storage/KafkaStoreSSLNoAuthTest.java
@@ -0,0 +1,88 @@
+/**
+ * Copyright 2014 Confluent Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.confluent.kafka.schemaregistry.storage;
+
+import io.confluent.kafka.schemaregistry.SSLClusterTestHarness;
+import io.confluent.kafka.schemaregistry.storage.exceptions.StoreException;
+import io.confluent.kafka.schemaregistry.storage.exceptions.StoreInitializationException;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+public class KafkaStoreSSLNoAuthTest extends SSLClusterTestHarness {
+  private static final Logger log = LoggerFactory.getLogger(KafkaStoreSSLNoAuthTest.class);
+
+  protected boolean requireSSLClientAuth() {
+    return false;
+  }
+
+  @Before
+  public void setup() {
+    log.debug("Zk conn url = " + zkConnect);
+  }
+
+  @After
+  public void teardown() {
+    log.debug("Shutting down");
+  }
+
+  @Test
+  public void testInitialization() {
+    KafkaStore<String, String> kafkaStore = StoreUtils.createAndInitSSLKafkaStoreInstance(zkConnect,
+            zkClient, clientSslConfigs, false);
+    kafkaStore.close();
+  }
+
+
+  @Test
+  public void testIncorrectInitialization() {
+    KafkaStore<String, String> kafkaStore = StoreUtils.createAndInitSSLKafkaStoreInstance(zkConnect,
+            zkClient, clientSslConfigs, false);
+    try {
+      kafkaStore.init();
+      fail("Kafka store repeated initialization should fail");
+    } catch (StoreInitializationException e) {
+      // this is expected
+    }
+    kafkaStore.close();
+  }
+
+  @Test
+  public void testSimplePut() throws InterruptedException {
+    KafkaStore<String, String> kafkaStore = StoreUtils.createAndInitSSLKafkaStoreInstance(zkConnect,
+            zkClient, clientSslConfigs, false);
+    String key = "Kafka";
+    String value = "Rocks";
+    try {
+      kafkaStore.put(key, value);
+    } catch (StoreException e) {
+      fail("Kafka store put(Kafka, Rocks) operation failed");
+    }
+    String retrievedValue = null;
+    try {
+      retrievedValue = kafkaStore.get(key);
+    } catch (StoreException e) {
+      fail("Kafka store get(Kafka) operation failed");
+    }
+    assertEquals("Retrieved value should match entered value", value, retrievedValue);
+    kafkaStore.close();
+  }
+}
